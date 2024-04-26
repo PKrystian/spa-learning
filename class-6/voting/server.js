81263f494e2768282c5b8c8f009d83edc4cfdc46
PKrystian <krystian.pinczak2001@gmail.com>
2024-04-26 14:20:02 +0200

    added files for class 6 (ws)

--- class-6/voting/server.js
+++ class-6/voting/server.js
@@ -1,17 +1,35 @@
 const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+
 let results = {
     pizza: 0,
     pasta: 0
 };
-const wsServer = new WebSocket.Server({ port: 3000 });
+
+const app = express();
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server: server, path: "/voting" });
+app.use(express.static('client'));
+server.listen(3000);
+
+const clients = new Set();
+
 wsServer.on('connection', (socket) => {
+    clients.add(socket);
     console.log('Client connected!');
     socket.on('message', (message) => {
         // handle vote
+        if (message == 'pizza') {
+            results.pizza++;
+        } else if (message == 'pasta') {
+            results.pasta++;
+        }
         console.log('Message received: ' + message);
         wsServer.clients.forEach(function (client) {
             if (client.readyState === WebSocket.OPEN) {
                 // send current results to all clients
+                client.send(JSON.stringify(results));
                 console.log('Sending to a client from clients.');
             }
         });
@@ -20,5 +38,11 @@ wsServer.on('connection', (socket) => {
         console.log('Error:' + error);
     });
     // send current results to a newly connected client
+    socket.send(JSON.stringify(results));
     console.log('Sending to a newly connected client.');
+    socket.on('close', () => {
+        clients.delete(socket);
+        console.log('Client disconnected, total number of clients is: ',
+            clients.size);
+    });
 });
